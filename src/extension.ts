a15fa8b93089baf5a104babe73d004fa2a81f4cf
Jokinko <jozefchmelar@users.noreply.github.com>
2022-09-19 17:15:05 +0200

    MVP Working (#1)
    * MVP Working

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,16 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	let compareTabs = vscode.commands.registerCommand('compare.compareTabs', async () => {
-		vscode.window.showInformationMessage('Hello World from compare!');
-
-		let what = await vscode.window.showInputBox({ placeHolder: 'cow say?' });
-
+	context.globalState.get("accessor");
+ 	let compareTabs = vscode.commands.registerCommand(  'compare.compareTabs',  async (eventParams) => {
+		var uriCurrent = vscode.window.activeTextEditor?.document.uri;
+		var uriSelected = eventParams
+        vscode.commands.executeCommand('vscode.diff', uriCurrent, uriSelected , 'name of the tab');
 	});
-
-	let backwards = vscode.commands.registerCommand('compare.backwards', async () => {
-	});
-
-
 	context.subscriptions.push(compareTabs);
-	context.subscriptions.push(backwards);
-
-	
-
 }
 
 // this method is called when your extension is deactivated
